79ebbac53d7555474d370ea43def991d53f2c512
nightcoding-ai <64195967+nightcoding-ai@users.noreply.github.com>
2020-04-27 16:45:15 +0200

    Update insertion-donnees.sql

--- backend/scripts-base-de-donnees/insertion-donnees.sql
+++ backend/scripts-base-de-donnees/insertion-donnees.sql
@@ -1,1 +1,40 @@
 /**Données**/
+
+INSERT INTO night.Categorie (idCat, categorie)
+VALUES 
+(01, 'chaises'),
+(02, 'armoires'),
+(03, 'tables'),
+(04, 'lits'),
+(05, 'bureux')
+;
+
+INSERT INTO night.Produits(idProd, produit, categId)
+VALUES 
+(100,'aman', 01),
+(101,'senem', 01),
+(102,'ribuk', 01),
+(103,'cjitul', 01),
+(104,'ajax', 01),
+(105,'leval', 02),
+(106,'arbot', 02),
+(107,'catux', 02),
+(108,'vidor', 02),
+(109,'suckle', 02),
+(110,'tubon', 03),
+(111,'mayi', 03),
+(112,'lidhx', 03),
+(113,'qutu', 03),
+(114,'chodol', 03),
+(115,'luxs', 04),
+(116,'timlbr', 04),
+(117,'shyc', 04),
+(118,'bonrr', 04),
+(119,'tvbosl', 04),
+(120,'buduc', 05),
+(121,'dafurofk', 05),
+(122,'amklid', 05),
+(123,'gjup', 05),
+(124,'redld', 05)
+;
+  
