206b766bb8ad5be990bdb4bf752c1d8343740e04
nightcoding-ai <64195967+nightcoding-ai@users.noreply.github.com>
2020-04-27 16:07:19 +0200

    Update creation-tables.sql

--- backend/scripts-base-de-donnees/creation-tables.sql
+++ backend/scripts-base-de-donnees/creation-tables.sql
@@ -1,1 +1,18 @@
 
+CREATE TABLE Produits 
+(
+  "idProd" INTEGER PRIMARY KEY NOT NULL, 
+  "produit" VARCHAR(20) NOT NULL,
+  "prodCat" INTEGER NOT NULL,
+  CONSTRAINT FK_Produits (prodCat)
+  REFERENCES Categories (idCat)
+ )
+ 
+ CREATE TABLE Categorie
+ (
+   "idCat" INTEGER PRIMARY KEY NOT NULL,
+   "categorie" VARCHAR(20) NOT NULL,
+ )
+  
+  
+  
